6c76d199a3a1e9c0d28771d541427a1c4a6ed063
Markus Thulin <macky_b@hotmail.com>
2023-02-12 21:28:51 +0100

    Starting to setup configuration

---
+++ PlaywrightFramework/Core/BrowserOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PlaywrightFramework.Core
+{
+    public class BrowserOptions
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = BrowserType.Chromium;
+        [JsonPropertyName("headed")]
+        public string Headed { get; set; } = "0";
+    }
+}
