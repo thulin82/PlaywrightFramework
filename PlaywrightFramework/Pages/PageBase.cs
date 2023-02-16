6b7222091ff13a97ed36d553c237ceb39d8bf5c6
Markus Thulin <macky_b@hotmail.com>
2023-02-16 17:47:34 +0100

    Added logging in PageBase

--- PlaywrightFramework/Pages/PageBase.cs
+++ PlaywrightFramework/Pages/PageBase.cs
@@ -1,8 +1,9 @@
-﻿using NUnit.Framework.Internal;
+﻿using NLog;
 
 namespace PlaywrightFramework.Pages
 {
-    public abstract class PageBase
+    //[TestFixture]
+    public abstract class PageBase : IDisposable, IAsyncDisposable
     {
         protected readonly IPage page;
         protected static readonly ILogger logger;
@@ -17,29 +18,58 @@ namespace PlaywrightFramework.Pages
             this.page.Crash += Page_Crash;
         }
 
+        static PageBase()
+        {
+            LogManager.LoadConfiguration("nLog.config");
+            logger = LogManager.GetCurrentClassLogger();
+        }
+
+        /*[SetUp]
+        public void SetUp()
+        {
+            logger.Debug($"Test: '{TestContext.CurrentContext.Test.FullName}'");
+        }
+        */
+
         private void Page_Crash(object? sender, IPage e)
         {
-            throw new NotImplementedException();
+            logger.Debug($"Crashed page URL is {e.Url}");
         }
 
         private void Page_PageError(object? sender, string e)
         {
-            throw new NotImplementedException();
+            logger.Error(e);
         }
 
         private void Page_Console(object? sender, IConsoleMessage e)
         {
-            throw new NotImplementedException();
+            logger.Debug(e.ToString());
         }
 
         private void Page_Close(object? sender, IPage e)
         {
-            throw new NotImplementedException();
+            logger.Debug($"Closed page URL is {e.Url}");
         }
 
         private void Page_Load(object? sender, IPage e)
         {
-            throw new NotImplementedException();
+            logger.Debug($"Loaded page URL is {e.Url}");
+        }
+
+        public void Dispose()
+        {
+            this.page.Load -= Page_Load;
+            this.page.Close -= Page_Close;
+            this.page.Console -= Page_Console;
+            this.page.PageError -= Page_PageError;
+            this.page.Crash -= Page_Crash;
+            GC.SuppressFinalize(this);
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            Dispose();
+            return ValueTask.CompletedTask;
         }
     }
 }
