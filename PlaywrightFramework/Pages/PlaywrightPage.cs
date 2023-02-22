5092a968827b5856ba42dc5a2c82d9f8aea7c10f
Markus Thulin <macky_b@hotmail.com>
2023-02-22 20:51:14 +0100

    First test up and running

---
+++ PlaywrightFramework/Pages/PlaywrightPage.cs
@@ -0,0 +1,22 @@
+﻿namespace PlaywrightFramework.Pages
+{
+    public class PlaywrightPage : PageBase
+    {
+        private readonly ILocator _locator;
+
+        public PlaywrightPage(IPage page) : base(page)
+        {
+            _locator = page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+        }
+
+        public async Task GoTo()
+        {
+            await page.GotoAsync("/");
+        }
+
+        public async Task ClickGetStarted()
+        {
+            await _locator.ClickAsync();
+        }
+    }
+}
--- PlaywrightFramework/Tests/Test.cs
+++ PlaywrightFramework/Tests/Test.cs
@@ -9,29 +9,11 @@ public class Test : TestBase
     [Test]
     public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
-        using (var searchPage = new SearchPage(Page))
-        {
-            await searchPage.GoTo();
-            await searchPage.Search("Markus Thulin");
-        }
-
-        /*await Page.GotoAsync("https://playwright.dev");
-
-        // Expect a title "to contain" a substring.
+        PlaywrightPage page = new PlaywrightPage(Page);
+        await page.GoTo();
         await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
+        await page.ClickGetStarted();
         await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
-        // Expects the title to contain Installation
-        await Expect(Page).ToHaveTitleAsync(new Regex(".*Installation"));*/
+        await Expect(Page).ToHaveTitleAsync(new Regex(".*Installation"));
     }
 }
