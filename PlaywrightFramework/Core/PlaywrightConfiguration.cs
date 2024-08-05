fb32c24e96f134c6dd37340119fca961b32fa108
Markus Thulin <macky_b@hotmail.com>
2024-08-05 11:49:15 +0200

    Added some null checks

--- PlaywrightFramework/Core/PlaywrightConfiguration.cs
+++ PlaywrightFramework/Core/PlaywrightConfiguration.cs
@@ -4,13 +4,17 @@ namespace PlaywrightFramework.Core;
 
 public class PlaywrightConfiguration
 {
-    public readonly BrowserOptions browserOptions;
-    public readonly BrowserNewContextOptions browserNewContextOptions;
+    public readonly BrowserOptions? browserOptions;
+    public readonly BrowserNewContextOptions? browserNewContextOptions;
 
     public PlaywrightConfiguration()
     {
         var config = JsonReader.Read<Config>("configuration.json");
-        browserOptions = config.Browser;
-        browserNewContextOptions = config.BrowserContextOptions;
+        if (config != null)
+        {
+            browserOptions = config.Browser;
+            browserNewContextOptions = config.BrowserContextOptions;
+        }
+
     }
 }
